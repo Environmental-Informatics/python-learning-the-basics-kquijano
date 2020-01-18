af361da84b3bdfb03a15e2d0b9c9d70fdbf93c9d
Karoll Jessenia Quijano Escalante <kquijano@scholar-fe00.rcac.purdue.edu>
2020-01-17 20:24:08 -0500

    Lab 01

---
+++ Kquijano_Exercise_3.3.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Karoll Quijano - kquijano
+
+ABE 651 - Environmental Informatics
+
+Think Python - Chapter 3
+Exercise 3.3
+
+"""
+
+# Exercise 3.3.1  - draw a 2*2 grid
+
+def print_four (f):
+    ''' Prints four times a function '''
+    f()
+    f()
+    f()
+    f()
+
+
+def print_two(f,g):
+    ''' Print two functions '''
+    f()
+    print_four(g)
+
+
+def print_first ():
+    ''' prints rows with + - '''
+    print('+ - - - -', end = ' ')
+    print('+ - - - - +' )
+    
+def print_last ():
+    ''' Prints rows with | '''
+    print('|        ', end = ' ')
+    print('|         |' )
+
+def do_box():
+    ''' Combines functions to print a 2*2 box '''
+    print_two(print_first, print_last)
+    print_two(print_first, print_last)
+    print_first()
+    
+    
+do_box()
+
+
+# Exercise 3.3.2  - draw a 4*4 grid  
+
+def print_first2 ():
+    ''' prints rows with + - '''
+    print(*3*('+----'), end = ' ')
+    print('+ - - - - +' )
+    
+def print_last2 ():
+    ''' Prints rows with | '''
+    print(*3*('|    '), end = ' ')
+    print('|         |' )
+
+
+
+def do_box4():
+    ''' Combines functions to print a 4*4 box '''
+    print_two(print_first2, print_last2)
+    print_two(print_first2, print_last2)
+    print_two(print_first2, print_last2)
+    print_two(print_first2, print_last2)
+    print_first2()
+    
+   
+do_box4()
